b3d7aab66def9216c129aa9d993e314561fddad2
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-18 21:12:19 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,15 @@
 def oxford_comma(array)
-  if array.length > 2 
-  array.join(',')
+  str = ""
+  counter = 0
+  if array.length == 1 
+  return array[0]
+elseif array.length == 2
+  return "#{array[0]} and #{array[1]}"
 else
-  more_than_two_elements = array.join(',')
-  return more_than_two_elements + 'and' 
+  until counter == array.length - 1 
+  str << "#{array[counter]}, "
+  i+=1
+end
+str << "and #{array[array.length - 1]}"
+return str
 end
