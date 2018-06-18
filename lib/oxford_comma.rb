95e00df956b809d9477a2f7a8cfd0a64c1125450
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-18 21:14:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,15 +2,15 @@ def oxford_comma(array)
   str = ""
   counter = 0
   if array.length == 1 
-  return array[0]
-elseif array.length == 2
-  return "#{array[0]} and #{array[1]}"
-else
-  until counter == array.length - 1 
-  str << "#{array[counter]}, "
-  i+=1
-end
-str << "and #{array[array.length - 1]}"
-return str
-end
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    until counter == array.length - 1 
+      str << "#{array[counter]}, "
+      i+=1
+    end
+    str << "and #{array[array.length - 1]}"
+    return str
+  end
 end
