02219e39b41034cda9ab2d4ca06f08061d87dcbd
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-18 21:15:08 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  str = ""
+  counter = 0
+  if array.length == 1 
+    return array[0]
+  elsif array.length == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    until counter == array.length - 1 
+      str << "#{array[counter]}, "
+      counter+=1
+    end
+    str << "and #{array[array.length - 1]}"
+    return str
+  end
 end
